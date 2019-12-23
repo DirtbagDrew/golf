2138e41ea212b7c6a902f9e7771afe7ba0caa3a1
DirtbagDrew <andrewmyerhb@gmail.com>
2019-12-22 21:08:28 -0800

    formatting

--- lib/widgets/VideoPlayerController.dart
+++ lib/widgets/VideoPlayerController.dart
@@ -7,10 +7,7 @@ import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 class VideoPlayerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Video Player Demo',
-      home: VideoPlayerScreen(),
-    );
+    return VideoPlayerScreen();
   }
 }
 
@@ -93,70 +90,67 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      // title: Text('Butterfly Video'),
-      // ),
-      // Use a FutureBuilder to display a loading spinner while waiting for the
-      // VideoPlayerController to finish initializing.
-      body: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the VideoPlayerController has finished initialization, use
-            // the data it provides to limit the aspect ratio of the video.
-            return AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              // Use the VideoPlayer widget to display the video.
-              child: VideoPlayer(_controller),
-            );
-          } else {
-            // If the VideoPlayerController is still initializing, show a
-            // loading spinner.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: SpeedDial(
-        // child: Icon(Icons.add, color: Colors.black),
-        // both default to 16
-        marginRight: 18,
-        marginBottom: 20,
-        animatedIcon: AnimatedIcons.menu_close,
-        animatedIconTheme: IconThemeData(size: 22.0),
-        // this is ignored if animatedIcon is non null
-        child: Icon(Icons.add),
-        visible: true,
-        // If true user is forced to close dial manually
-        // by tapping main button and overlay is not rendered.
-        closeManually: false,
-        curve: Curves.bounceIn,
-        overlayColor: Colors.black,
-        overlayOpacity: 0.5,
-        onOpen: () => print('OPENING DIAL'),
-        onClose: () => print('DIAL CLOSED'),
-        tooltip: 'Speed Dial',
-        heroTag: 'speed-dial-hero-tag',
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        elevation: 8.0,
-        shape: CircleBorder(),
-        children: [
-          SpeedDialChild(
-              child: Icon(Icons.video_library),
-              backgroundColor: Colors.red,
-              label: 'gallery',
-              labelStyle: TextStyle(fontSize: 18.0),
-              onTap: () => _pickVideoFromGallery()),
-          SpeedDialChild(
-            child: Icon(Icons.videocam),
-            backgroundColor: Colors.blue,
-            label: 'camera',
-            labelStyle: TextStyle(fontSize: 18.0),
-            onTap: () => _pickVideoFromCamera(),
+    return MaterialApp(
+        title: 'Video Player Demo',
+        home: Scaffold(
+          // Use a FutureBuilder to display a loading spinner while waiting for the
+          // VideoPlayerController to finish initializing.
+          body: FutureBuilder(
+            future: _initializeVideoPlayerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // If the VideoPlayerController has finished initialization, use
+                // the data it provides to limit the aspect ratio of the video.
+                return AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  // Use the VideoPlayer widget to display the video.
+                  child: VideoPlayer(_controller),
+                );
+              } else {
+                // If the VideoPlayerController is still initializing, show a
+                // loading spinner.
+                return Center(child: CircularProgressIndicator());
+              }
+            },
           ),
-        ],
-      ),
-    );
+          floatingActionButton: SpeedDial(
+            // child: Icon(Icons.add, color: Colors.black),
+            // both default to 16
+            marginRight: 18,
+            marginBottom: 20,
+            animatedIcon: AnimatedIcons.menu_close,
+            animatedIconTheme: IconThemeData(size: 22.0),
+            // this is ignored if animatedIcon is non null
+            child: Icon(Icons.add),
+            visible: true,
+            // If true user is forced to close dial manually
+            // by tapping main button and overlay is not rendered.
+            closeManually: false,
+            curve: Curves.bounceIn,
+            overlayColor: Colors.black,
+            overlayOpacity: 0.5,
+            tooltip: 'Speed Dial',
+            heroTag: 'speed-dial-hero-tag',
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            elevation: 8.0,
+            shape: CircleBorder(),
+            children: [
+              SpeedDialChild(
+                  child: Icon(Icons.video_library),
+                  backgroundColor: Colors.red,
+                  label: 'gallery',
+                  labelStyle: TextStyle(fontSize: 18.0),
+                  onTap: () => _pickVideoFromGallery()),
+              SpeedDialChild(
+                child: Icon(Icons.videocam),
+                backgroundColor: Colors.blue,
+                label: 'camera',
+                labelStyle: TextStyle(fontSize: 18.0),
+                onTap: () => _pickVideoFromCamera(),
+              ),
+            ],
+          ),
+        ));
   }
 }
